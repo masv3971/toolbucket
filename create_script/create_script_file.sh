951c005221be392d0dd030e2694a9aac4db36eaf
Magnus Svensson <magnux@gmail.com>
2018-07-14 01:34:33 +0000

    Going with python3.

--- create_script/create_script_file.sh
+++ create_script/create_script_file.sh
@@ -21,7 +21,7 @@ touch $script_name
 chmod +x $script_name
 
 if [[ $script_ending == "py" ]]; then
-    echo "#!/usr/bin/env python" > $script_name
+    echo "#!/usr/bin/env python3" > $script_name
 
 elif  [[ $script_ending == "pl" ]]; then
     echo "#!/usr/bin/env perl" > $script_name
